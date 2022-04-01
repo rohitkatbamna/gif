21aad21cf07e3f9306578fc788ae94c229508280
rohit katbamna <rohitkatbamna@protonmail.com>
2022-04-01 12:03:38 +0530

    11:44 AM COMMIT NO.2 GIF BUTTON ADDED

--- src/components/gif.js
+++ src/components/gif.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 
 function Gif() {
 	const [gif, setGif] = useState([]);
+	const [offset, setOffset] = useState(0);
 	const api = "sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh";
 	const givedata = {
 		method: "GET",
 	};
 	function Fetching() {
 		console.log("Begin Fetching");
-		fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${api}`, givedata)
+		fetch(
+			`https://api.giphy.com/v1/gifs/trending?api_key=${api}&limit=5&offset=${offset}`,
+			givedata
+		)
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
@@ -17,7 +21,7 @@ function Gif() {
 	}
 	useEffect(() => {
 		Fetching();
-	}, []);
+	}, [offset]);
 
 	return (
 		<div className="container-fluid">
@@ -34,6 +38,14 @@ function Gif() {
 						</div>
 					);
 				})}
+			</div>
+			<div className="row">
+				<button
+					type="button"
+					className="btn btn-primary col mt-3"
+					onClick={() => setOffset((prev) => prev + 5)}>
+					Next
+				</button>
 			</div>
 		</div>
 	);
--- src/components/modalbody.js
+++ src/components/modalbody.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import profileimg from "../assets/user.png";
 import "../scss/modalbody.scss";
 import Gif from "./gif";
 
 function Modalbody() {
+	const [wantgif, setWantgif] = useState(false);
 	return (
 		<>
 			<div className="modal_body_all_of_it">
@@ -15,12 +16,16 @@ function Modalbody() {
 				/>
 				<div contentEditable="true" style={{ border: "4px solid black" }}>
 					<input style={{ border: "none" }} />
-					<img src="https://media2.giphy.com/media/AwEhQFqm9KiHGPjdL5/giphy.gif?cid=9f0f6425vyjrhwka3wuturxn4h19e1mlsp3h8elvdmhnw0pv&rid=giphy.gif&ct=g" />
 				</div>
 			</div>
 			<div className="mt-5">
-				<h1>Gifs that you might want to select</h1>
-				<Gif />
+				<button
+					type="button"
+					className="btn btn-primary"
+					onClick={() => setWantgif(wantgif ? false : true)}>
+					Gif
+				</button>
+				{wantgif ? <Gif /> : <></>}
 			</div>
 		</>
 	);
